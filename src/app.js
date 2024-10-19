79e58f1c7c73c406bc00733498dc9d1dda2c6065
wgbk <doankhanh12@gmail.com>
2024-10-19 11:13:34 +0700

    init project

---
+++ src/app.js
@@ -0,0 +1,25 @@
+const compression = require('compression')
+const express = require('express')
+const { default: helmet } = require('helmet')
+const morgan = require('morgan')
+const app = express()
+
+// init middlewares
+app.use(morgan("dev"))  //dev, combined (PROD use), common, short, tiny
+app.use(helmet())
+app.use(compression())
+
+
+// init db
+
+
+// init routes
+app.get('/', (req, res, next) => {
+    return res.status(200).json({
+        message: 'Welcome!'
+    })
+})
+
+// handle error
+
+module.exports = app
