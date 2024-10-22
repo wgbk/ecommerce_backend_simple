3ad593405ef47b081e02d096538ea7ef852f071c
wgbk <doankhanh12@gmail.com>
2024-10-22 16:37:48 +0700

    add api key

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,12 @@
 'use strict'
 
 const express = require('express')
+const { apiKey, permission } = require('../auth/checkAuth')
 const router = express.Router()
 
+router.use(apiKey)
+router.use(permission('0000'))
+
 router.use('/v1/api', require('./access'))
-// router.get('', (req, res, next) => {
-//   return res.status(200).json({
-//     message: "Welcome!",
-//   });
-// });
 
 module.exports = router
